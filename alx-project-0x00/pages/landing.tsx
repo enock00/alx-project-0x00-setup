5c7fa200520f6c63086521197163518eedc1b7e8
Enock <enockzy54@gmail.com>
2025-09-30 20:08:03 +0300

    creating an button component that accepts title and styles as an argument.

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,21 +10,21 @@ export default function Landing() {
       <div className="flex gap-4">
         <Button title="Small - sm" styles="text-sm px-2 py-1 rounded-sm" />
         <Button title="Small - md" styles="text-sm px-2 py-1 rounded-md" />
-        <Button title="Small - full" styles="text-sm px-2 py-1 rounded-lg" />
+        <Button title="Small - full" styles="text-sm px-2 py-1 rounded-full" />
       </div>
 
       {/* Medium Buttons */}
       <div className="flex gap-4">
         <Button title="Medium - sm" styles="text-base px-4 py-2 rounded-sm" />
         <Button title="Medium - md" styles="text-base px-4 py-2 rounded-md" />
-        <Button title="Medium - full" styles="text-base px-4 py-2 rounded-lg" />
+        <Button title="Medium - full" styles="text-base px-4 py-2 rounded-full" />
       </div>
 
       {/* Large Buttons */}
       <div className="flex gap-4">
         <Button title="Large - sm" styles="text-lg px-6 py-3 rounded-sm" />
         <Button title="Large - md" styles="text-lg px-6 py-3 rounded-md" />
-        <Button title="Large - full" styles="text-lg px-6 py-3 rounded-lg" />
+        <Button title="Large - lg" styles="text-lg px-6 py-3 rounded-full" />
       </div>
     </div>
   );
