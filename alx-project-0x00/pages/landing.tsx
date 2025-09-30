ad9d308a2e9913bf84e5f45bc72bf1db82228be1
Enock <enockzy54@gmail.com>
2025-09-30 20:14:54 +0300

    added rounded-lg

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,6 +10,7 @@ export default function Landing() {
       <div className="flex gap-4">
         <Button title="Small - sm" styles="text-sm px-2 py-1 rounded-sm" />
         <Button title="Small - md" styles="text-sm px-2 py-1 rounded-md" />
+        <Button title="Small - lg" styles="text-sm px-2 py-1 rounded-lg" />
         <Button title="Small - full" styles="text-sm px-2 py-1 rounded-full" />
       </div>
 
@@ -17,6 +18,7 @@ export default function Landing() {
       <div className="flex gap-4">
         <Button title="Medium - sm" styles="text-base px-4 py-2 rounded-sm" />
         <Button title="Medium - md" styles="text-base px-4 py-2 rounded-md" />
+        <Button title="Medium - lg" styles="text-lg px-6 py-3 rounded-lg" />
         <Button title="Medium - full" styles="text-base px-4 py-2 rounded-full" />
       </div>
 
@@ -24,6 +26,7 @@ export default function Landing() {
       <div className="flex gap-4">
         <Button title="Large - sm" styles="text-lg px-6 py-3 rounded-sm" />
         <Button title="Large - md" styles="text-lg px-6 py-3 rounded-md" />
+        <Button title="Large - lg" styles="text-lg px-6 py-3 rounded-lg" />
         <Button title="Large - lg" styles="text-lg px-6 py-3 rounded-full" />
       </div>
     </div>
